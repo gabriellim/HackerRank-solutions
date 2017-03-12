927679cd86cde502046225615da660498e7c1b6a
gabriellim <gabriel_lim@sfu.ca>
2017-03-12 11:42:13 -0700

    Finished Day 5,6

---
+++ 30 Days of Code/Day 5 - Loops/Solution.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        
+        for (int i = 1; i <= 10; i++) {
+            String result = String.format("%d x %d = %d", n, i, n*i);
+            System.out.println(result);
+        }
+    }
+}
---
+++ 30 Days of Code/Day 6 - Let's Review/Solution.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        
+        while (n > 0) {
+            String str = scan.nextLine();
+            StringBuilder evenChars = new StringBuilder();
+            StringBuilder oddChars = new StringBuilder();
+            
+            for (int i = 0; i < str.length(); i++) {
+                char[] charArray = str.toCharArray();
+                
+                if (i%2 == 0) {
+                    evenChars.append(charArray[i]);
+                }
+                else {
+                    oddChars.append(charArray[i]);
+                }
+            }
+            
+            String result = String.format("%s %s", evenChars, oddChars);
+            System.out.println(result);
+            
+            n -= 1;
+        }
+        
+    }
+}
