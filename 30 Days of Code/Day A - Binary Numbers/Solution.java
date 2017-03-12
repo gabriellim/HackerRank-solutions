0f64593d343b3bf3bdb59b3b33a4a8f4556e0400
gabriellim <gabriel_lim@sfu.ca>
2017-03-12 16:41:21 -0700

    Closed scanner

--- 30 Days of Code/Day A - Binary Numbers/Solution.java
+++ 30 Days of Code/Day A - Binary Numbers/Solution.java
@@ -6,6 +6,7 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = Integer.parseInt(scan.nextLine());
+        scan.close();
         
         int max = 0, count = 0;
         while (n > 0) {
