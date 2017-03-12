49e6f0f90dee89d0ff02bf90f6bf082cb1e607a8
gabriellim <gabriel_lim@sfu.ca>
2017-03-12 16:39:27 -0700

    Finished Day 10

---
+++ 30 Days of Code/Day A - Binary Numbers/Solution.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        
+        int max = 0, count = 0;
+        while (n > 0) {
+            int remainder = n%2;
+            n = n/2;
+            
+            if (remainder == 1) {
+                count += 1;
+            }
+            else {
+                count = 0;
+            }
+            
+            if (count > max) {
+                max = count;
+            }
+        }
+        System.out.println(max);
+    }
+}
