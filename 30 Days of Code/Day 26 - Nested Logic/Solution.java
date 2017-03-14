e6a8f29dcf47ac0823713471113b041f414b0f7e
Gabriel Lim <gabriel_lim@sfu.ca>
2017-03-14 01:44:42 -0700

    Updated Day 26

--- 30 Days of Code/Day 26 - Nested Logic/Solution.java
+++ 30 Days of Code/Day 26 - Nested Logic/Solution.java
@@ -1,23 +1,33 @@
 import java.util.*;
 import java.math.*;
 
+class BookReturnDate {
+    public int day, month, year;
+    
+    BookReturnDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+}
+
 public class Solution {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int actualDay = scan.nextInt(), actualMonth = scan.nextInt(), actualYear = scan.nextInt();
-        int expectedDay = scan.nextInt(), expectedMonth = scan.nextInt(), expectedYear = scan.nextInt();
+        BookReturnDate actual = new BookReturnDate(scan.nextInt(), scan.nextInt(), scan.nextInt());
+        BookReturnDate expected = new BookReturnDate(scan.nextInt(), scan.nextInt(), scan.nextInt());
         scan.close();
         
         int fine;
-        if (actualYear > expectedYear) {
+        if (actual.year > expected.year) {
             fine = 10000;
         }
-        else if (actualYear == expectedYear && actualMonth > expectedMonth) {
-            fine = 500*Math.abs(actualMonth - expectedMonth);
+        else if (actual.year == expected.year && actual.month > expected.month) {
+            fine = 500*Math.abs(actual.month - expected.month);
         }
-        else if (actualMonth == expectedMonth && actualDay > expectedDay) {
-            fine = 15*Math.abs(actualDay - expectedDay);
+        else if (actual.month == expected.month && actual.day > expected.day) {
+            fine = 15*Math.abs(actual.day - expected.day);
         }
         else {
             fine = 0;
