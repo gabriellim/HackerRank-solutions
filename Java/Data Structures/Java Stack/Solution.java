ea5d45742fa5eb02cac8c8ee3484bf5b1ef3f4f7
Gabriel Lim <gabriel_lim@sfu.ca>
2017-03-22 17:00:15 -0700

    Finished Java Stack

---
+++ Java/Data Structures/Java Stack/Solution.java
@@ -0,0 +1,48 @@
+import java.util.*;
+class Solution {
+
+    static final HashMap<Character,Character> hmap = new HashMap<Character,Character>();
+    static {
+        hmap.put('[', ']');
+        hmap.put('{', '}');
+        hmap.put('(', ')');
+    }
+
+    public static void main(String []args) {
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNext()) {
+            String str = scan.nextLine();
+            System.out.println(bracketMatch(str));
+        }
+    }
+
+    static boolean bracketMatch(String str) {
+        Stack<Character> myStack = new Stack<Character>();
+
+        for (char left : str.toCharArray()) {
+            if (!Character.isLetterOrDigit(left)) {
+                if (hmap.containsKey(left)) {
+                    myStack.push(left);
+                }
+                else {
+                    if (!myStack.empty()) {
+                        char right = hmap.get(myStack.peek());
+
+                        if (left == right) {
+                            myStack.pop();
+                        }
+                    }
+                    else {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        if (myStack.empty()) {
+            return true;
+        }
+
+        return false;
+    }
+}
