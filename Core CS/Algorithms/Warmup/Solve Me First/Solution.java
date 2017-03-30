88d9674ff8db879ab16a27cd2ca4d6b992972055
Gabriel Lim <gabriel_lim@sfu.ca>
2017-03-29 23:19:02 -0700

    Finished Algo - Solve Me First

---
+++ Core CS/Algorithms/Warmup/Solve Me First/Solution.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    static int solveMeFirst(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int sum = solveMeFirst(a,b);
+        System.out.println(sum);
+    }
+}
--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 
 ### Core CS
 * [Algorithms](https://www.hackerrank.com/domains/algorithms)
-  * - [ ] [Warmup](https://www.hackerrank.com/domains/algorithms/warmup) (6/9)
+  * - [ ] [Warmup](https://www.hackerrank.com/domains/algorithms/warmup) (7/9)
 
 ### Programming Languages
 * [Java](https://www.hackerrank.com/domains/java)
