586ef99bf9f8ab7bd18fb5495b59522b0cef68d8
Gabriel Lim <gabriel_lim@sfu.ca>
2017-04-05 15:09:35 -0700

    Finished 30DoC Day 28 - RegEx

---
+++ 30 Days of Code/Day 28 - RegEx/Solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        ArrayList<String> al = new ArrayList<String>();
+
+        for (int i = 0; i < n; i++) {
+            String firstName = scan.next();
+            String emailID = scan.next();
+
+            Pattern p = Pattern.compile("@gmail.com");
+            Matcher m = p.matcher(emailID);
+
+            if (m.find()) {
+                al.add(firstName);
+            }
+        }
+
+        Collections.sort(al);
+
+        for (String fname : al) {
+            System.out.println(fname);
+        }
+    }
+}
